0b425a8229a36a3ba83d33ee6743056dc5fb5a58
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-05-26 18:49:13 +0900

    :sparkles: Make page title font size responsive using the clamp() function

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -15,7 +15,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
 
 PageTitle.css = `
 .page-title {
-  font-size: 1.75rem;
+  font-size: clamp(1.2rem, 6vw, 1.75rem);
   margin: 0;
   font-family: var(--titleFont);
 }
